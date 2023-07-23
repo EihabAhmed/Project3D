662941fa78fbdd3c1eb708cba9797c88193ff3ca
EihabAhmed <eihabahmed@gmail.com>
2023-07-23 11:53:35 +0300

    Initial Commit

---
+++ android/src/com/mygdx/project3d/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.mygdx.project3d;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.mygdx.project3d.Project3D;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new Project3D(), config);
+	}
+}
---
+++ desktop/src/com/mygdx/project3d/DesktopLauncher.java
@@ -0,0 +1,15 @@
+package com.mygdx.project3d;
+
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+import com.mygdx.project3d.Project3D;
+
+// Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+		config.setForegroundFPS(60);
+		config.setTitle("Project 3D");
+		new Lwjgl3Application(new Project3D(), config);
+	}
+}
